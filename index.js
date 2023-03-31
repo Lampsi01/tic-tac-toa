4f37b8628345932a022fcdd8288c0b228e085dd5
lotfi belabbas <111601982+Lampsi01@users.noreply.github.com>
2023-03-31 11:03:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@
         text.innerHTML=gameState[7] +" is the winner"
         winner = true; 
         disable(); 
-     } else if(gameState[1] !="" && gameState[1] !="" && gameState[3] != "" && gameState[4] !="" && gameState[5] !="" && gameState[6] != "" && gameState[6] != "" && gameState[7] !="" && gameState[8] != "" && gameState[9] !="" && winner==false){
+     } else if(gameState[1] !="" && gameState[2] !="" && gameState[3] != "" && gameState[4] !="" && gameState[5] !="" &&  gameState[6] != "" && gameState[7] !="" && gameState[8] != "" && gameState[9] !="" && winner==false){
         disable()
         text.innerHTML= "it A draw"; 
 
